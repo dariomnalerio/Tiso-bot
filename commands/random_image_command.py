44417451d8aac08d97b74bdf2b2f56b2be670dc3
dariomnalerio <dariomnalerio@gmail.com>
2023-08-21 05:42:08 -0300

    Refactor: GetRandomUrl to API module

--- commands/random_image_command.py
+++ commands/random_image_command.py
@@ -5,18 +5,6 @@ import uuid
 from api.random_image import GetRandomUrl
 
 
-def GetRandomUrl():
-    """Get random image url from the database"""
-    from database.database import get_session
-    from sqlalchemy.sql import func
-    from models.random_image_model import RandomImage
-
-    session = get_session()
-    random_url = session.query(RandomImage.url).order_by(func.random()).first()
-    session.close()
-    return random_url
-
-
 def randomImage(bot):
     @bot.command()
     async def randomImage(ctx):
